921b3c1e541e1f8ebdcc5b004f9e71ac1db9fce9
simratbenipal <117254852+simratbenipal@users.noreply.github.com>
2022-11-10 21:49:49 -0700

    commit from vs code

--- admin_editUserInfo.php
+++ admin_editUserInfo.php
@@ -127,7 +127,7 @@
 				catch (Exception $e)
 				{
 					echo "<p class = \"error\">";
-					echo "Error, please check the data and enter again</p>";
+					echo "Error,fgsdgsdgsdg please check the data and enter again</p>";
 					//header("Location: admin_editUserInfo.php");
 				}	
 			}
